f0d25c4d9b09c8ff613f14a928e9d6b801f6df94
odysian <odyssian7@gmail.com>
2026-03-17 12:29:57 -0400

    chore: cleanup stale docs and update gitignore

--- backend/render-build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-pip install -r requirements.txt
-
-# Run migrations (creates quaero schema + tables, same pattern as Rostra)
-alembic upgrade head
